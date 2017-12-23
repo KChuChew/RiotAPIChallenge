a7cf2ad344238621f89ab674e69cc8604f026558
Danny Yang <danny.dyang@gmail.com>
2017-12-22 18:45:17 -0800

    added champion api call

---
+++ rune_helper/app/controllers/champion_controller.rb
@@ -0,0 +1,7 @@
+class ChampionController < ApplicationController
+	def index
+		@url = 'https://na1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&dataById=false&api_key=RGAPI-91cb05bc-76d3-46f1-bcdb-ffea08832129'
+		@champ_data = nil 
+		@response = nil
+	end
+end
