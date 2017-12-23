b5cfbd0bf30e051321c9e7a0e47a14b11a059623
Kevin Chu <kevin.git.chu@gmail.com>
2017-12-23 10:40:36 -0800

    Added link to Rune Page

--- rune_helper/app/controllers/champion_controller.rb
+++ rune_helper/app/controllers/champion_controller.rb
@@ -1,11 +1,13 @@
 class ChampionController < ApplicationController
 	def index
-		@url = 'https://na1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&dataById=false&api_key=RGAPI-91cb05bc-76d3-46f1-bcdb-ffea08832129'
+		key1 = 'RGAPI-91cb05bc-76d3-46f1-bcdb-ffea08832129'
+		key2 = 'RGAPI-b633a05e-8f92-4376-a914-84a530c8a71c'
+		@url = 'https://na1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&dataById=false&api_key=' + key2
 		@champ_data = nil 
 		@response = nil
 	end
 
-	def champ_page
-
+	def rune_page
+		@runes = 'dank'
 	end
 end
--- rune_helper/config/routes.rb
+++ rune_helper/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'champion#index'
-  get "../app/views/champion/dank.html.erb", to: "champion#champ_page", as: "champ_page"
+  get '/champion/runes', to: 'champion#rune_page', as: 'runes'
+  #get '/champion/', to:'champion#rune_page', as: 'runes'
 end
