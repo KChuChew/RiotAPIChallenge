cb401165348ea7255fdc3b9798455bf062a3b455
Kevin Chu <kevin.git.chu@gmail.com>
2017-12-23 23:47:22 -0800

    Example Extraction for Rune IDs

--- rune_helper/app/controllers/champion_controller.rb
+++ rune_helper/app/controllers/champion_controller.rb
@@ -2,12 +2,19 @@ class ChampionController < ApplicationController
 	def index
 		key1 = 'RGAPI-91cb05bc-76d3-46f1-bcdb-ffea08832129'
 		key2 = 'RGAPI-b633a05e-8f92-4376-a914-84a530c8a71c'
-		@url = 'https://na1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&dataById=false&api_key=' + key2
+		@url = 'https://na1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&dataById=false&api_key=' + key1
 		@champ_data = nil 
 		@response = nil
 	end
 
 	def show 
 		@champ_name = params[:id]
+	end
+
+	def champgg
+		ggkey = '36cecc2b9b2b8c380d1c56b403fc300a'
+		@url_b = 'http://api.champion.gg/v2/champions?elo=BRONZE&champData=hashes&limit=1&api_key=' + ggkey
+		@champ_data_b = nil
+		@ggresponse = nil
 	end
 end
--- rune_helper/config/routes.rb
+++ rune_helper/config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'champion#index'
-<<<<<<< HEAD
-  get '/champion/runes', to: 'champion#rune_page', as: 'runes'
+  root 'champion#champgg'
+  #get '/champion/runes', to: 'champion#rune_page', as: 'runes'
   #get '/champion/', to:'champion#rune_page', as: 'runes'
-=======
 
   resources :champion
   resources :runes
 
->>>>>>> ab94e951b133a20c4f5fa805dc1fd943c1043094
 end
